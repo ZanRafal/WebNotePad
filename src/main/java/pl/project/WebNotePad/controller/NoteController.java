34ed90aa7135c403bfe9efb9c0227e9ba8935c7a
ZanRafal <01159225@pw.edu.pl>
2021-09-19 18:15:42 +0200

    Added readNote method

--- src/main/java/pl/project/WebNotePad/controller/NoteController.java
+++ src/main/java/pl/project/WebNotePad/controller/NoteController.java
@@ -6,12 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.project.WebNotePad.model.Note;
 import pl.project.WebNotePad.model.NoteRepository;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,6 +31,13 @@ public class NoteController {
     ResponseEntity<List<Note>> readAllNotes (Pageable page) {
         logger.warn("Displaying list of notes with Pageable");
         return ResponseEntity.ok(noteRepository.findAll(page).getContent());
+    }
+
+    @GetMapping("/notes/{id}")
+    ResponseEntity<Note> readNote(@PathVariable int id) {
+        return noteRepository.findById()
+                .map(note -> ResponseEntity.ok(note))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 
